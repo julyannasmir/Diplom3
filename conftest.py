c4afbc7faed7c0bf490d842d4bba5861c6edbcc5
Julianna Smirnova <julyannasmir@gmail.com>
2024-06-28 13:01:35 +0300

    changes

--- conftest.py
+++ conftest.py
@@ -1,13 +1,13 @@
 import pytest
 from selenium import webdriver
-import helpers
+import helper
 import requests
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from urls import Url
 
 
-@pytest.fixture(params=["chrome"])
+@pytest.fixture(params=["firefox"])
 def driver(request):
     if request.param == 'chrome':
         driver = webdriver.Chrome()
@@ -22,7 +22,7 @@ def driver(request):
 
 @pytest.fixture()
 def user_data():
-    user_data = helpers.generate_user_data()
+    user_data = helper.generate_user_data()
     response = requests.post(Url.REGISTER_USER, user_data)
     yield user_data
     headers = {'Authorization': response.json()['accessToken']}
